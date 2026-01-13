16c995dbee1e55768f51148a2e1ec75c5edd1cba
Zx-TEKP1X-xZ <datenamilgraux@gmail.com>
2026-01-13 04:36:47 -0300

    Trilha sonora na tela inicial e botao mutar/desmutar

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//Começo botão  jogar
 const botao = document.getElementById("botaodeInicio");
 
 function iniciar (){
@@ -5,3 +6,26 @@ function iniciar (){
 }
 
 botao.addEventListener("click", iniciar);
+// Fim botão jogar
+
+//Começo musica de fundo
+document.getElementById("audio").volume = 0.3;
+
+const botaoMutar = document.getElementById("muteAudioButton");
+let mutar = document.getElementById("audio");
+let verificarSom = false
+
+function mutarAudio (){
+    if (verificarSom == false) {
+        mutar.muted = true;
+        verificarSom = true;
+    }
+
+    else if (verificarSom == true){
+        mutar.muted = false;
+        verificarSom = false;
+    }
+}
+
+botaoMutar.addEventListener("click", mutarAudio);
+//Fim música de fundo
